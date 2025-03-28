c7dbbf3c7e219ed76ffd469f30cd7279036fca5e
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2025-03-28 13:31:16 +0100

    childless

--- internal/provider/inverse_cidr_function_test.go
+++ internal/provider/inverse_cidr_function_test.go
@@ -125,10 +125,10 @@ func TestInverseCidrFunction_Invalid(t *testing.T) {
 			childCIDR:  "172.16.0.0/24",
 			error:      `(?s)Call to function "provider::iactools::inverse_cidr" failed.*Error calculating.*inverse CIDRs: child CIDR 172.16.0.0/24 is not within parent CIDR.*192.168.0.0/16`,
 		},
-		"mask-too-large": {
+		"childless-parent-cidr": {
 			parentCIDR: "192.168.84.42/32",
 			childCIDR:  "192.168.84.42/32",
-			error:      `(?s)Call to function "provider::iactools::inverse_cidr" failed.*Error calculating.*inverse CIDRs: child CIDR 172.16.0.0/24 is not within parent CIDR.*192.168.0.0/16`,
+			error:      `(?s)Call to function "provider::iactools::inverse_cidr" failed.*Error calculating.*inverse CIDRs: child CIDR not found within parent CIDR`,
 		},
 	}
 
