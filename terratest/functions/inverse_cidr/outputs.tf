e107995a456ba1a7af1d068c056251e7db91d333
Bence Bánó <18106157+Ledermayer@users.noreply.github.com>
2026-05-05 19:53:51 +0200

    Rename output to inverse_cidrs and update value

--- terratest/functions/inverse_cidr/outputs.tf
+++ terratest/functions/inverse_cidr/outputs.tf
@@ -2,6 +2,6 @@
 # SPDX-FileCopyrightText: The terraform-provider-iactools Authors
 # SPDX-License-Identifier: MPL-2.0
 
-output "inverse_cidr" {
-  value = provider::iactools::inverse_cidr(var.parent_cidr, var.child_cidr)
+output "inverse_cidrs" {
+  value = provider::iactools::inverse_cidrs(var.parent_cidr, var.child_cidr)
 }
